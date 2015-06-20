f221a9fd6d3c4390ef02e3509c0ac34fc7a3af39
Max Kaye <m@xk.io>
2015-06-20 16:08:38 +1000

    seeknbuild using asyncio

--- cryptonet/seeknbuild.py
+++ cryptonet/seeknbuild.py
@@ -1,6 +1,7 @@
 import time
 import queue
 import threading
+import asyncio
 
 from cryptonet.datastructs import *
 from cryptonet.debug import debug, verbose_debug
@@ -25,7 +26,7 @@ class SeekNBuild:
     See the block_seeker and chain_builder functions for more info.
     '''
 
-    def __init__(self, p2p, chain):
+    def __init__(self, p2p, chain, event_loop=None):
         self.p2p = p2p
         self.chain = chain
         self.chain.learn_of_seek_n_build(self)
@@ -33,27 +34,24 @@ class SeekNBuild:
         self.nonces = AtomicIncrementor()
 
         self.future = set()
-        self.future_queue = queue.PriorityQueue()
+        self.future_queue = asyncio.PriorityQueue()
         self.present = set()
-        self.present_queue = queue.PriorityQueue()
+        self.present_queue = asyncio.PriorityQueue()
         self.past = set()
-        self.past_queue = queue.PriorityQueue()
-        self.past_queue_no_parent = queue.PriorityQueue()
+        self.past_queue = asyncio.PriorityQueue()
+        self.past_queue_no_parent = asyncio.PriorityQueue()
         self.done = set()
         self.all = set()
         self._shutdown = False
-
-        self.future_lock = threading.Lock()
-        self.present_lock = threading.Lock()
-        self.past_lock = threading.Lock()
 
         self._funcs = {
             'height': self.chain.get_height,
         }
 
-        self.threads = [threading.Thread(target=self.block_seeker), threading.Thread(target=self.chain_builder)]
-        for t in self.threads:
-            t.start()
+        self._loop = asyncio.new_event_loop() if event_loop is None else event_loop
+        asyncio.async(self.block_seeker(), loop=self._loop)
+        asyncio.async(self.block_reseeker(), loop=self._loop)
+        asyncio.async(self.chain_builder(), loop=self._loop)
 
     def max_blocks_at_once(self):
         # no reason for special values besides
@@ -61,28 +59,32 @@ class SeekNBuild:
 
     def shutdown(self):
         self._shutdown = True
-        for t in self.threads:
-            t.join()
 
     def seek_hash_now(self, block_hash):
         ''' Add block_hash to queue with priority -1 (will be pulled next).
         '''
-        if block_hash == 0: return
-        if block_hash not in self.all:
-            with self.future_lock:
+        @asyncio.coroutine
+        def do(block_hash):
+            if block_hash == 0:
+                return
+            if block_hash not in self.all:
                 self.future_queue.put((-1, block_hash))
                 self.future.add(block_hash)
+                self.all.add(block_hash)
+        asyncio.async(do(block_hash), loop=self._loop)
 
     def seek_with_priority(self, block_hash_with_height):
         ''' Add block_hash to future queue with its priority.
         '''
-        height, block_hash = block_hash_with_height
-        if block_hash == 0: return
-        if block_hash not in self.all:
-            self.all.add(block_hash)
-            with self.future_lock:
+        @asyncio.coroutine
+        def do(block_hash_with_height):
+            height, block_hash = block_hash_with_height
+            if block_hash == 0: return
+            if block_hash not in self.all:
+                self.all.add(block_hash)
                 self.future_queue.put((height, block_hash))
                 self.future.add(block_hash)
+        asyncio.async(do(block_hash_with_height), loop=self._loop)
 
     def seek_many_with_priority(self, block_hashes_with_height):
         ''' Applies each in list to seek_with_priority()
@@ -90,74 +92,68 @@ class SeekNBuild:
         for height, block_hash in block_hashes_with_height:
             self.seek_with_priority((height, block_hash))
 
-    def block_seeker(self):
-        ''' block_seeker() should be in its own thread.
-        block_seeker will run in a loop and:
+    @asyncio.coroutine
+    def block_reseeker(self):
+        '''
         1. Are there any blocks that were requested more than X seconds ago
-            1.1 for each block that was, add it to requesting
-            1.2 throw it away if it is no longer in the self.preset set
+            1.1 throw it away if it is no longer in the self.present set
+            1.2 for each block that was, add it to requesting
+        '''
+        requesting = IntList()
+        while not self._shutdown:
+            to_get = min(len(self.future), self.max_blocks_at_once()) - requesting.len()
+            oldest_timestamp, oldest_block_hash = yield from self.present_queue.get()
+            if oldest_timestamp + 10 < time.time() and to_get > 0:  # requested >10s ago
+                if oldest_block_hash in self.present:
+                    if self.chain.has_block_hash(oldest_block_hash):
+                        self.present.remove(oldest_block_hash)
+                    else:
+                        debug('seeker, block re-request: %064x' % oldest_block_hash)
+                        requesting.append(oldest_block_hash)
+            else:
+                yield from self.present_queue.put((oldest_timestamp, oldest_block_hash))  # put hash back in the queue
+                if len(requesting) > 0:
+                    self.p2p.broadcast(b'request_blocks', requesting.serialize())
+                    for hash in requesting:
+                        self.present_queue.put_nowait((time.time(), hash))
+                    requesting = IntList()
+                yield from asyncio.sleep(0.5)
+
+    @asyncio.coroutine
+    def block_seeker(self):
+        ''' block_seeker() is run through asyncio.
+        block_seeker will run in a loop and:
         2. Find the number of blocks to send
             2.1 get that many from the future_queue
         3. For each block_hash to request, add it to the present_queue with the time it was requested.
         4. Pick a random peer and send the request to it.
         '''
         while not self._shutdown and not self.chain.initialized:
-            time.sleep(0.1)
+            yield from asyncio.sleep(0.1)
+
         while not self._shutdown:
-            # we will eventually serialize this so we make it a Field
-            requesting = IntList()
+            requesting = IntList()  # encodium object
 
-            try:
-                with self.present_lock:
-                    oldest_timestamp, oldest_block_hash = self.present_queue.get_nowait()
-                    while oldest_timestamp + 10 < time.time():  # requested >10s ago
-                        if oldest_block_hash in self.present:
-                            if not self.chain.has_block_hash(oldest_block_hash):
-                                debug('seeker, block re-request: %064x' % oldest_block_hash)
-                                requesting.append(oldest_block_hash)
-                            else:
-                                if oldest_block_hash in self.present:
-                                    self.present.remove(oldest_block_hash)
+            to_get = min(len(self.future), self.max_blocks_at_once())
+            if to_get > 0:
+                # pick some blocks to request
+                for i in range(to_get):
+                    priority, h = self.future_queue.get_nowait()
+                    #print('block_seeker: asking for height: ',_)
+                    self.future.remove(h)
+                    if priority != 0:  # note: why exclude priority 0? genesis block?
+                        requesting.append(h)
 
-                        oldest_timestamp, oldest_block_hash = self.present_queue.get_nowait()
-                    self.present_queue.put((oldest_timestamp, oldest_block_hash))
-            except queue.Empty:
-                pass
-
-            with self.future_lock:
-                to_get = min(len(self.future), self.max_blocks_at_once()) - requesting.len()
-                if to_get > 0:
-                    # pick some blocks to request
-                    for i in range(to_get):
-                        _, h = self.future_queue.get()
-                        #print('block_seeker: asking for height: ',_)
-                        if h in self.future:
-                            self.future.remove(h)
-                        if _ != 0:
-                            requesting.append(h)
-
-                for h in requesting:
-                    with self.present_lock:
-                        self.present_queue.put((int(time.time()), h))
-                        self.present.add(h)
+            for h in requesting:
+                self.present_queue.put_nowait((int(time.time()), h))
+                self.present.add(h)
 
             if requesting.len() > 0:
                 # TODO : don't broadcast to all nodes, just one
                 print(requesting.serialize())
                 self.p2p.broadcast(b'request_blocks', requesting.serialize())
-                # Borked in spore since asyncio
-                '''some_peer = self.p2p.random_peer()
-                while True:
-                    # ordered carefully
-                    if some_peer == None:
-                        time.sleep(0.01)
-                        some_peer = self.p2p.random_peer()
-                    else:
-                        break
-                some_peer.send('request_blocks', requesting.serialize())
-                some_peer.data['lastmessage'] = time.time()'''
             else:
-                time.sleep(0.05)
+                yield from asyncio.sleep(0.1)
 
     def get_chain_height(self):
         return self._funcs['height']()
@@ -170,23 +166,23 @@ class SeekNBuild:
         Add a block to the past_queue (ready for chain_builder) if we haven't done so before.
         '''
         # blocks should be internally consistent at this point
-        block_hash = block.get_hash()
-        to_put = (block.height, self.nonces.get_next(), block)
+        @asyncio.coroutine
+        def _add_block(block):
+            block_hash = block.get_hash()
+            to_put = (block.height, self.nonces.get_next(), block)
 
-        with self.past_lock:
             if block_hash in self.past or block_hash in self.done:
                 return
             self.past.add(block_hash)
-            self.past_queue.put(to_put)
+            self.past_queue.put_nowait(to_put)
 
-        self.all.add(block_hash)
+            self.all.add(block_hash)
 
-        with self.present_lock:
-            try:
+            if block_hash in self.present:
                 self.present.remove(block_hash)
-            except KeyError:
-                pass
+        asyncio.async(_add_block(block), loop=self._loop)
 
+    @asyncio.coroutine
     def chain_builder(self):
         '''
         1. Get the next block.
@@ -197,17 +193,16 @@ class SeekNBuild:
             2.4 Broadcast to peers
         '''
         while not self._shutdown and not self.chain.initialized:
-            time.sleep(0.1)
+            yield from asyncio.sleep(0.1)
+
         while not self._shutdown:
-            try:
-                with self.past_lock:
-                    height, nonce, block = self.past_queue.get(timeout=0.1)
-            except queue.Empty:
-                continue
+            height, nonce, block = yield from self.past_queue.get()
+
             if block.height == 0:
                 self.past.remove(block.get_hash())
                 self.done.add(block.get_hash())
                 continue
+
             block_hash = block.get_hash()
             #print('chain_builder: checking %d' % block.height)
             # TODO : handle orphans intelligently
@@ -218,27 +213,24 @@ class SeekNBuild:
                 with self.past_lock:
                     self.past_queue.put((height, nonce, block))
                     while not self.past_queue_no_parent.empty():
-                        self.past_queue.put(self.past_queue_no_parent.get())
-                time.sleep(0.05)
+                        self.past_queue.put_nowait(self.past_queue_no_parent.get_nowait())
+                yield from asyncio.sleep(0.05)
             else:
                 if self.chain.has_block(block_hash):
-                    try:
-                        self.past.remove(block_hash)
-                    except KeyError:
-                        pass
+                    self.past.remove(block_hash)
                     self.done.add(block_hash)
                     continue
+
                 # TODO : handle orphans intelligently
                 if not self.chain.has_block_hash(block.parent_hash):
                     debug('chain_builder: don\'t have parent')
                     debug('chain_builder: head and curr', self.chain.head.get_hash(), block.parent_hash)
-                    with self.past_lock:
-                        self.past_queue_no_parent.put((height, nonce, block))
+                    self.past_queue_no_parent.put_nowait((height, nonce, block))
                     continue
+
                 # todo: only broadcast block on success
-                with self.past_lock:
-                    self.past.remove(block_hash)
-                    self.done.add(block_hash)
+                self.past.remove(block_hash)
+                self.done.add(block_hash)
                 self.chain.add_block(block)
                 debug('builder to send : %064x' % block.get_hash())
                 to_send = BlocksMessage(contents=[block.serialize()])
